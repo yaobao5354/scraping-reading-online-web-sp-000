2ba185aa068184a96521fe30d2db7054bf1036d8
yaoyu bao <yaobao5354@gmail.com>
2019-09-09 19:25:12 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,12 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
 
+puts doc.css(".site-header__hero__headline").text.strip
 
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course| 
+  puts course.text.strip 
+end 
